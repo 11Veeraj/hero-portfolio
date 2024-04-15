8dfa98b6f7a073f767a5a788e03326668cab6da9
Veeraj Goudar <veeraj@Veerajs-Macbook-Pro.local>
2024-04-15 13:05:55 +0530

    Updates

---
+++ static/zTrCjxuSLVlWPMXiLLpJP/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/78-ede1c6b206a8b9ac.js","static/css/b04218d57dd918ef.css","static/chunks/pages/index-b2c87a264d38d2da.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
